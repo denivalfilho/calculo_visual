b06e527f209b8672b818518b43989a92ac736787
Denival Biotto Filho <denivaldenival@gmail.com>
2026-05-25 12:09:34 -0300

    Update integral.py

--- integral.py
+++ integral.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def f(x):
-    return x**2 + 1
-
-a = 0
-b = 3
+def soma_de_riemann(f, a, b, n):
+    largura = (b - a) / n
+    x_ret = np.linspace(a, b - largura, n)
+    area = np.sum(largura * f(x_ret))
+    return area
