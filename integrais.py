b05620f619a4b240f418f49c8bd911505d4ac3f8
Denival Biotto Filho <denivaldenival@gmail.com>
2026-05-26 16:33:37 -0300

    Update integrais.py

--- integrais.py
+++ integrais.py
@@ -23,3 +23,5 @@ def gerar_grafico(f, a, b, n, expressao="f(x)"):
     area_riemann = soma_de_riemann(f, a, b, n)
     area_exata = integral_exata(f, a, b)
     erro = abs(area_exata - area_riemann)
+
+    fig, ax = plt.subplots(figsize=(9, 6))
